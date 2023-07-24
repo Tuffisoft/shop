48f03dcb98ac49f327803087593e8cfbce8f1a47
Tuffisoft <vancetti@gmail.com>
2023-07-24 10:32:54 +0200

    "add backend connect to frontend"

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import data from "./data.js";
 
 const app = express();
+app.use(cors());
 
 app.get("/api/products", (req, res) => res.send(data.products));
 
